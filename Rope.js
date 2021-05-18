1a30719e032d79a1c7eb77664b96c00e441dd48e
SwapnilSuman07 <74098375+SwapnilSuman07@users.noreply.github.com>
2021-05-18 12:16:50 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,29 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+    display(){
+        if(this.rope.bodyA != null){
+
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.rope.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+    fly(){
+        this.rope.bodyA= null;
+    }
+    
+    attach(body)
+     { this.rope.bodyA = body;}
+}
